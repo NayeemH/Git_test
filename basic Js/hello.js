245a182335105ece7e3b099fc5aef1e50a7ef178
NayeemH <nayeemh67@gmail.com>
2021-08-02 23:11:14 +0600

    conditional,date,array,array method,while loop

---
+++ basic Js/hello.js
@@ -0,0 +1,6 @@
+var age = 15
+
+while(age < 25){
+    console.log("You are not eligible for covid vaccine");
+    age = age+3;
+}
--- basic Js/index.js
+++ basic Js/index.js
@@ -1,3 +1,6 @@
-var a = Math.random()*1000;
-changeA= Math.round(a)
-console.log(changeA)
+
+var upperBoundary = 20;
+var lowerBoundary = 10;
+var randomNumber = Math.round(Math.random() * (upperBoundary - lowerBoundary + 1)) + lowerBoundary;
+console.log(randomNumber);
+
